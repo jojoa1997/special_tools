5ac073564b6fba461ecadf43d4c9fc2a1dbed259
jojoa1997 <fake@earthlink.net>
2013-01-18 16:08:27 -0500

    added crow bar

--- init.lua
+++ init.lua
@@ -27,8 +27,33 @@ minetest.register_craft({
 	}
 })
 
-
---Aliases
-minetest.register_alias("shears", "special_tools:shears")
+--	
+--Crowbar	
+--
+minetest.register_tool("special_tools:crowbar", {	
+	description = "Normal Crowbar",
+	inventory_image = "crowbar.png",	
+	wield_image = "crowbar.png",
+	stack_max = 1,
+	max_drop_level=3,
+	tool_capabilities = {
+		full_punch_interval = 1.4,
+		max_drop_level=3,
+		groupcaps={
+			cracky={times={[1]=3.0, [2]=3.0, [3]=0.72}, uses=10, maxlevel=3},
+			crumbly={times={[1]=3.0, [2]=3.0, [3]=0.72}, uses=10, maxlevel=3},	
+			snappy={times={[1]=3.0, [2]=3.0, [3]=0.72}, uses=10, maxlevel=3}	
+			}
+		}
+	})
+	
+minetest.register_craft({
+	output = 'special_tools:crowbar',
+	recipe = {
+		{'default:steel_ingot', 'default:stick', ''},
+		{'', 'default:mese', ''},
+		{'', 'default:stick', 'default:steel_ingot'},
+	}
+})
 
 print("special_tools loaded! by jojoa1997 and Likwid H-Craft")
