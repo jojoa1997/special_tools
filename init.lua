84f2b5a97e6ec93c0498ef487fb319ac03a32553
jojoa1997 <fake@earthlink.net>
2013-01-18 16:10:21 -0500

    fix indentation

--- init.lua
+++ init.lua
@@ -4,17 +4,17 @@
 
 minetest.register_tool("special_tools:shears", {
 	description = "Shears",
-    inventory_image = "shears.png",
-    wield_image = "shears.png",
-    stack_max = 1,
+	inventory_image = "shears.png",
+	wield_image = "shears.png",
+	stack_max = 1,
 	max_drop_level=3,
-    tool_capabilities = {
-        full_punch_interval = 1.0,
-        max_drop_level=0,
-        groupcaps={
-            snappy={times={[3]=0.2}, maxwear=0.05, maxlevel=3},
+	tool_capabilities = {
+	        full_punch_interval = 1.0,
+	        max_drop_level=0,
+	        groupcaps={
+			snappy={times={[3]=0.2}, maxwear=0.05, maxlevel=3},
 			wool={times={[3]=0.2}, maxwear=0.05, maxlevel=3}
-        }
+        	}
     }
 })
 
